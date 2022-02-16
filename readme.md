607cc9d2652586e0eb893af9998ac28bce993bec
Jordan Hansen <jbhansen84@gmail.com>
2022-02-16 14:15:26 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,45 @@
+# Best Proxies for Web Scraping in 2022
+
+Today we compare four different proxies in two different categoies: speed and success rate. We'll test on both an easy site and a very difficult one.
+
+The four proxies that we will be testing are:
+
+- [ScrapingBee](https://www.scrapingbee.com?fpr=jordan-hansen52)
+- [Luminati/BrightData](https://brightdata.grsm.io/jordanhansen6276)
+- [ScraperAPI](https://www.scraperapi.com?fpr=jbsg)
+- [ProxyCrawl](https://proxycrawl.com/?s=ukQB9Fqz)
+
+## Getting Started
+
+Clone the repository and run `npm i`.
+
+You will need a API keys for each service. Sign up above to get the keys.
+
+Update the `.sample.env` file with your keys and rename it to `.env`.
+
+`npm start` to run it.
+
+### Prerequisites
+
+Tested on Node v16.13.2 and NPM v8.1.2
+
+### Installing
+
+After installing [NodeJS](https://nodejs.org/en/) you should be able to just run the following in the terminal.
+
+```
+npm i
+```
+
+## Built With
+
+* [axios](https://github.com/axios/axios) - Axios
+
+## Authors
+
+* **Jordan Hansen** - *Initial work* - [Jordan Hansen](https://github.com/aarmora)
+
+
+## License
+
+This project is licensed under the ISC License
---
+++ src/index.ts
@@ -0,0 +1,153 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import cheerio from 'cheerio';
+import HttpsProxyAgent from 'https-proxy-agent';
+import { performance } from 'perf_hooks';
+
+dotenv.config();
+
+(async () => {
+    // await scrapingFastPeopleSearchLuminati();
+    // await scrapingFastPeopleSearchScrapingBee();
+    await scrapingFastPeopleSearchScraperAPI();
+    // await scrapingFastPeopleSearchProxyCrawl();
+})();
+
+
+// FastPeopleSearch uses heavy bot detection and hCatpcha
+async function scrapingFastPeopleSearchScrapingBee() {
+    // With render_js off
+    // Need to use premium_proxy for best results - 10 credits per request
+    let errors = 0;
+    let times = [];
+    for (let i = 0; i < 20; i++) {
+        const startTime = performance.now();
+        let endTime;
+        try {
+            // const url = 'https://www.fastpeoplesearch.com/name/john-doe_biloxi-ms';
+            const url = 'http://pizza.com';
+            const axiosResponse = await axios.get('https://app.scrapingbee.com/api/v1', {
+                params: {
+                    'api_key': process.env.scrapingBeeApiKey,
+                    'url': url,
+                    render_js: false,
+                    premium_proxy: true,
+                    country_code: 'us'
+                }
+            });
+            endTime = performance.now();
+            const $ = cheerio.load(axiosResponse.data);
+
+            console.log('axiosResponse', $('h1').text(), i);
+            const time = (endTime - startTime) / 1000;
+            console.log('time', time);
+            times.push(time);
+        }
+        catch (e) {
+            endTime = performance.now();
+            // ScrapingBee will throw a 500 when it hits the catpcha/403 error status
+            console.log('error, we assume failure');
+            errors++;
+        }
+    }
+    console.log('total errors', errors);
+    console.log('times', times, 'average time', average(times));
+}
+
+// FastPeopleSearch uses heavy bot detection and hCatpcha
+async function scrapingFastPeopleSearchScraperAPI() {
+    // Need to use premium=true for best results - 10 credits per request
+    let errors = 0;
+    let times = [];
+    for (let i = 0; i < 20; i++) {
+        const startTime = performance.now();
+        let endTime;
+        // const url = 'https://www.fastpeoplesearch.com/name/john-doe_biloxi-ms';
+        const url = 'http://pizza.com';
+        try {
+            const axiosResponse = await axios.get(`http://api.scraperapi.com?api_key=${process.env.scraperApiApiKey}&url=${encodeURIComponent(url)}&premium=true`);
+            endTime = performance.now();
+            const $ = cheerio.load(axiosResponse.data);
+
+            console.log('axiosResponse', $('h1').text(), i);
+            const time = (endTime - startTime) / 1000;
+            console.log('time', time);
+            times.push(time);
+        }
+        catch (e) {
+            endTime = performance.now();
+            console.log('error, we assume failure');
+            errors++;
+        }
+    }
+    console.log('total errors', errors);
+    console.log('times', times, 'average time', average(times));
+}
+
+// FastPeopleSearch uses heavy bot detection and hCatpcha
+async function scrapingFastPeopleSearchProxyCrawl() {
+    // Need to use premium=true for best results - 10 credits per request
+    let errors = 0;
+    let times = [];
+    for (let i = 0; i < 20; i++) {
+        const startTime = performance.now();
+        let endTime;
+        const url = 'https://www.fastpeoplesearch.com/name/john-doe_biloxi-ms';
+        // const url = 'http://pizza.com';
+        try {
+            const axiosResponse = await axios.get(`https://api.proxycrawl.com/?token=${process.env.proxyCrawlApiKey}&url=${encodeURIComponent(url)}&premium=true`);
+            endTime = performance.now();
+            const $ = cheerio.load(axiosResponse.data);
+
+            console.log('axiosResponse', $('h1').text(), i);
+            const time = (endTime - startTime) / 1000;
+            console.log('time', time);
+            times.push(time);
+        }
+        catch (e) {
+            endTime = performance.now();
+            console.log('error, we assume failure');
+            errors++;
+        }
+    }
+    console.log('total errors', errors);
+    console.log('times', times, 'average time', average(times));
+}
+
+// FastPeopleSearch uses heavy bot detection and hCatpcha
+async function scrapingFastPeopleSearchLuminati() {
+    // Need to use premium=true for best results - 10 credits per request
+    let errors = 0;
+    let times = [];
+    for (let i = 0; i < 20; i++) {
+        const startTime = performance.now();
+        let endTime;
+        const url = 'https://www.fastpeoplesearch.com/name/john-doe_biloxi-ms';
+        // const url = 'http://pizza.com';
+        try {
+            const axiosResponse = await axios.get(url, {
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+                },
+                proxy: false,
+                httpsAgent: new HttpsProxyAgent.HttpsProxyAgent(`https://${process.env.luminatiUsername}:${process.env.luminatiPassword}@zproxy.lum-superproxy.io:22225`)
+            });
+            endTime = performance.now();
+            const $ = cheerio.load(axiosResponse.data);
+
+            console.log('axiosResponse', $('h1').text(), i);
+            const time = (endTime - startTime) / 1000;
+            console.log('time', time);
+            times.push(time);
+        }
+        catch (e) {
+            endTime = performance.now();
+            console.log('error, we assume failure');
+            errors++;
+        }
+    }
+    console.log('total errors', errors);
+    console.log('times', times, 'average time', average(times));
+}
+
+const average = (array) => array.reduce((a, b) => a + b) / array.length;
